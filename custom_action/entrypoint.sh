c9280c1ed9dbb07d4a13577e38b8edb609f889a9
Yulian Stefanov <80903580+yu-Stefanov@users.noreply.github.com>
2023-11-16 17:16:21 +0200

    Update entrypoint.sh

--- custom_action/entrypoint.sh
+++ custom_action/entrypoint.sh
@@ -1,7 +1,13 @@
+# Shebang line - this tells the OS which interpreter to use to run the script
 #!/bin/bash
+# Tells Bash to exit the script immediately if an error is encountered
 set -e
 
 # if keyword is found
+# the "$*" is variable that contains the argument that were passed to the script 
+# in this case we are echoing those args to a grep command that will look for the word fixed
+# the -i tells grep to ignore case
+# the -q tells grep to be quiet so no output gets printed
 if echo "$*" | grep -i -q FIXED;
 then
     # do something
@@ -11,3 +17,7 @@ else
     # exit gracefully
     echo "Nothing to process."
 fi
+
+
+# !!! run chmod +x <script name> to make it executable
+# run script like <script path> <arg>
